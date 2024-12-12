8e87cbae3c4e5bac40dc4a6041765ff87cdf0d51
Graeme Hawker <graeme.hawker@strath.ac.uk>
2024-12-12 03:46:11 +0000

    updated day 9

--- solution9.jl
+++ solution9.jl
@@ -1,19 +1,20 @@
 using IterTools
 
 function dense_to_map(dense)
-    id = 0
-    data_map = ""
-    for x in partition(collect(string(dense))[1:end-1],2)
-        data_map = data_map*string(id)^parse(Int,x[1])
-        data_map = data_map*"."^parse(Int,x[2])
-        id += 1
+    data_map = []
+    int_dense = [parse(Int,i) for i in collect(string(dense))]
+    for i in eachindex(int_dense)
+        if i % 2 == 1
+            append!(data_map, repeat([(i-1)÷2], int_dense[i]))
+        else
+            append!(data_map, repeat(['.'], int_dense[i]))
+        end
     end
-    data_map = data_map * string(id) ^ parse(Int,collect(string(dense))[end])
     return data_map
 end
 
 function check_complete(data_map)
-    return findfirst(==('.'), data_map) == length(data_map) - count(==('.'),collect(data_map)) + 1
+    return findfirst(==('.'), data_map) == length(data_map) - count(==('.'),data_map) + 1
 end
 
 function defrag(data_map)
@@ -28,20 +29,20 @@ end
 function get_checksum(data_map)
     total = 0
     for i in 1:findlast(!=('.'), data_map)
-        total += (i-1)*parse(Int, data_map[i])
+        total += (i-1)*data_map[i]
     end
     return total
 end
 
-data_map = dense_to_map("2333133121414131402")
-defrag_map = String(defrag(collect(data_map)))
+@show data_map = dense_to_map("2333133121414131402")
+@show defrag_map = defrag(data_map)
 @show checksum = get_checksum(defrag_map)
 
-#data_map = dense_to_map(read("inputs/input9.txt", String))
+data_map = dense_to_map(read("inputs/input9.txt", String))
+defrag_map = defrag(data_map)
+@show checksum = get_checksum(defrag_map)
+
+#data_map = dense_to_map("11111911111")
 #defrag_map = String(defrag(collect(data_map)))
 #@show checksum = get_checksum(defrag_map)
-
-data_map = dense_to_map("11111911111")
-defrag_map = String(defrag(collect(data_map)))
-@show checksum = get_checksum(defrag_map)
 
